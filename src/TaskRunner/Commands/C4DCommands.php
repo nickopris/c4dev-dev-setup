0c3ebd55ce1d9075f83b0b3824bb8ee38e917987
Nick Opris <oprisnicolae@gmail.com>
2024-04-10 09:36:01 +0200

    Create C4DCommands.php

---
+++ src/TaskRunner/Commands/C4DCommands.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace c4d-dev-commands\TaskRunner\Commands;
+
+use Robo\Tasks;
+
+class C4DCommands extends Tasks
+{
+  /**
+   * @command c4d-dev:setup
+   */
+  public function command() {}
+}
